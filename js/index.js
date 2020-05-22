01a53910b93ed4df30295e89ecae23d1f2ffa3e9
jona <jona@jona.com>
2020-05-23 02:18:39 +0300

    fixing the bugs with the reset

--- js/index.js
+++ js/index.js
@@ -2,30 +2,33 @@ const calculator_form=document.querySelector('form');
 
 const result_display=document.querySelector('#result');
 
+const clear_button=document.querySelector('#clear_button');
+
+
 
 
 const calculate_button=document.querySelector('#calc_button');
 
 const loan_display=document.querySelector('#loan');
-loan_display.innerHTML="loan Amount: ";
+// loan_display.innerHTML="loan Amount: ";
 
 const interest_display=document.querySelector('#interest');
-interest_display.innerHTML="Interest: ";
+// interest_display.innerHTML="Interest: ";
 // result_display.innerHTML="hello";
 
 const principle_display=document.querySelector('#principle-display');
-principle_display.innerHTML="Principle: ";
+// principle_display.innerHTML="Principle: ";
 
 
 calculator_form.addEventListener('submit',calculateTheLoan);
 
 function calculateTheLoan(e){
 
-    const principle=document.querySelector('#principle').value;
+    let principle=document.querySelector('#principle').value;
 
-    const rate=document.querySelector('#rate').value;
+    let rate=document.querySelector('#rate').value;
 
-    const time=document.querySelector('#time').value;
+    let time=document.querySelector('#time').value;
 
     console.log(principle);
     console.log(time);
@@ -41,23 +44,43 @@ function calculateTheLoan(e){
         alert("Please enter a percentage");
     }
     
-    simple_interest=parseInt(principle) * (rate/100) * parseFloat(time/12);
+    simple_interest=parseInt(principle * (rate/100) * (time/12));
 
-    amount =simple_interest +parseInt(principle);
+    amount =parseInt(simple_interest +principle);
     
-    interest_display.innerHTML+=`<br> 
+    interest_display.innerHTML+`<br> 
                     <div class="amount">${simple_interest} </div>`; 
 
-    loan_display.innerHTML+=`<br> 
+    loan_display.innerHTML+`<br> 
                     <div class="amount">${amount} </div>`; 
 
     principle_display.innerHTML+=`<br> 
                     <div class="amount">${principle} </div>`; 
+    
+    
+    
+    clear_button.addEventListener('click',ResetAll);
+
+    function ResetAll(){
+        const amount=document.querySelectorAll('.amount');
+        for(var i=0; i<amount.length; i++){
+            amount[i].style.display="none";
+        }
+        principle=0;
+        rate=0;
+        time=0;
+
+        simple_interest=0;
+    
+    }
 
 
     
     e.preventDefault();
 }
+
+
+
 
 
 
